942d1eae95f648e44bb12f87a7f94b87bb6051a8
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2026-02-26 11:02:57 +0200

    Render basic pages for "/" and "/new"

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 import app from "./app.js";
+import express from "express";
 
 const PORT = process.env.APP_PORT || 3000;
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
+});
+
+app.get("/new", (req, res) => {
+    res.render("new");
 });
 
 app.listen(PORT, () => {
