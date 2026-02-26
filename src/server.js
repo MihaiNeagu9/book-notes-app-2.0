26c66f136e3c41179fbe1a9ec037349b428ea824
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2026-02-26 12:00:36 +0200

    Add path and fileURLToPath to resolve views/public paths reliably

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
-import app from "./app.js";
 import express from "express";
+import app from "./app.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const PORT = process.env.APP_PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicDir = path.join(__dirname, "..", "public");
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicDir));
 
 app.get("/", (req, res) => {
     res.render("index");
